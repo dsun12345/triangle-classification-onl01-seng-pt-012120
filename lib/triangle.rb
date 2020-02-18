726431ba77bd6cfbc9de0861166716137d8f3f63
Daniel Sun <danielsun12345@gmail.com>
2020-02-18 02:05:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,17 +8,25 @@ class Triangle
   end 
   
   def kind
+<<<<<<< HEAD
     if (@length == @width) && (@width == @height) 
       :equilateral
     elsif (@length == @width) || (@width == @height) || (@length == @height)  
       :isosceles
+=======
+    if @length == @width == @height 
+      :equilateral
+>>>>>>> 38cfa2f7a671d3852e4ae82a1018e40233448240
     else
       :scalene 
     end 
   end 
   
+<<<<<<< HEAD
     class TriangleError < StandardError
       
     end 
   
+=======
+>>>>>>> 38cfa2f7a671d3852e4ae82a1018e40233448240
 end
